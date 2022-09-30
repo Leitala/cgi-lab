d745f0d17649193d3530a32f060610c76707c556
Leitala <yhe7@ualberta.ca>
2022-09-30 16:00:30 -0600

    Update code

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+env = {}
+for env_key, env_value in os.environ.items():
+    env[env_key] = env_value
+
+print("Content-Type: apllication/json")
+print()
+
+
+
+print(json.dumps(env))
